ef9c4e98caaa8431dc099c7046894225c09bdc64
qiukai86263007 <4684390@qq.com>
2025-02-18 15:55:36 +0800

    bug修改，默认端口修改

--- agent/test.py
+++ agent/test.py
@@ -1,14 +1,10 @@
 from loguru import logger
 
-from utils.logger_manager import LoggerManager
+from utils.logger_manager import LogManager
 
 # 获取已配置的logger对象
-logger = LoggerManager.get_logger()
+logger = LogManager.get_logger()
 
-# 使用logger进行日志记录
-logger.info("这是一条测试日志信息")
-logger.debug("这是一条调试日志信息")
-logger.error("这是一条错误日志信息")
 
 if __name__ == '__main__':
     print("Hello, World!")
@@ -16,3 +12,5 @@ if __name__ == '__main__':
     # curl -X POST "http://localhost:8089/aihuman/task/anonymous/submit" -H "accept: */*" -H "Content-Type: multipart/form-data" -F "taskName=rest" -F "file=@test_audio.wav;type=audio/wav"
     # curl -X POST "http://localhost:8089/aihuman/task/anonymous/submit" -H "accept: */*" -H "Content-Type: multipart/form-data" -F "clientId=463bc68a-ce3a-415c-a9f7-c755921edaba" -F "materialId=sun" -F "parentTaskId=818c6df0-3f16-4bcc-b67e-ead92f31e134" -F "userId=kaikai" -F "file=@test_audio.wav;type=audio/wav"
     # curl -X GET "http://localhost:8089/aihuman/task/anonymous/download?taskUUID=test_audio" -H "accept: */*"
+    # prod env
+    # curl -i -X POST "http://60.165.239.28:1880/prod-api/aihuman/cluster/heartbeat/101"
